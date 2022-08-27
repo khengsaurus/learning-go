a3c4186bdb0cf993e894705fea2c8847e29067a6
tzikheng <tzikheng@gmail.com>
2022-08-27 16:14:29 +0800

    redis-caching
    - implement redis caching
    - add docker + local.env config

--- redis-caching/main.go
+++ redis-caching/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
+	"time"
 
 	"redis-caching/middleware"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
 )
+
+type API struct {
+	redisClient *redis.Client
+}
 
 type APIResponse struct {
 	Cache bool                `json:"cache"`
@@ -30,23 +42,57 @@ type NominatinLocation struct {
 }
 
 var (
-	port         = ":8080"
+	container    = os.Getenv("CONTAINER") == "true"
 	nominatimUrl = "https://nominatim.openstreetmap.org/search?q=%s&format=json"
+	defaultTTL   = time.Second * 300
 )
 
+func NewAPI() *API {
+	var opts *redis.Options
+
+	if container {
+		fmt.Printf("Using local redis service\n")
+		redisAddress := fmt.Sprintf("%s:6379", os.Getenv("REDIS_SERVICE"))
+		opts = (&redis.Options{
+			Addr:     redisAddress,
+			Password: "",
+			DB:       0,
+		})
+	} else {
+		fmt.Printf("Using remote redis service\n")
+		opts = (&redis.Options{
+			Addr:     os.Getenv("REDIS_URL"),
+			Password: os.Getenv("REDIS_PW"),
+			DB:       0,
+		})
+	}
+	rdb := redis.NewClient(opts)
+
+	return &API{redisClient: rdb}
+}
+
 func main() {
-	fmt.Printf("Starting server on port %s", port)
+	if !container {
+		envErr := godotenv.Load("local.env")
+		if envErr != nil {
+			panic(envErr)
+		}
+	}
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	fmt.Printf("Starting server on port %s\n", port)
+
+	api := NewAPI()
 	mux := http.NewServeMux()
 
-	handler := http.HandlerFunc(Handler)
+	handler := http.HandlerFunc(api.Handler)
 	mux.Handle("/api", middleware.UseJson(handler))
 
 	http.ListenAndServe(port, mux)
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func (api *API) Handler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query().Get("q")
-	data, err1 := GetData(q)
+	data, cacheHit, err1 := api.GetData(r.Context(), q)
 
 	if err1 != nil {
 		w.Write([]byte("Error calling Nominatim API"))
@@ -55,31 +101,63 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp := APIResponse{
-		Cache: false,
+		Cache: cacheHit,
 		Data:  data,
 	}
 
 	if err2 := json.NewEncoder(w).Encode(resp); err2 != nil {
-		fmt.Printf("Error encoding response: %v", err2)
+		fmt.Printf("Error encoding response: %v\n", err2)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 }
 
-func GetData(q string) ([]NominatinLocation, error) {
-	escapedQ := url.PathEscape(q)
-	url := fmt.Sprintf(nominatimUrl, escapedQ)
-	resp, err1 := http.Get(url)
-	if err1 != nil {
-		return nil, err1
-	}
+func (api *API) GetData(ctx context.Context, q string) (
+	[]NominatinLocation, bool, error) {
+	escapedQ := url.PathEscape(strings.ToLower(q))
+	cacheValue, redisErr := api.redisClient.Get(ctx, escapedQ).Result()
+	if redisErr == redis.Nil {
+		// Cached value does not exist. Call API -> get value -> cache
+		url := fmt.Sprintf(nominatimUrl, escapedQ)
+		resp, getErr := http.Get(url)
+		if getErr != nil {
+			return nil, false, getErr
+		}
 
-	data := make([]NominatinLocation, 0)
-	err2 := json.NewDecoder(resp.Body).Decode(&data)
-	if err2 != nil {
-		return nil, err2
-	}
+		data := make([]NominatinLocation, 0)
+		decodeErr := json.NewDecoder(resp.Body).Decode(&data)
+		if decodeErr != nil {
+			return nil, false, decodeErr
+		}
 
-	return data, nil
+		b, marshallErr := json.Marshal(data)
+		if marshallErr != nil {
+			return nil, false, marshallErr
+		}
+
+		redisSetErr := api.redisClient.Set(
+			ctx,
+			escapedQ,
+			bytes.NewBuffer(b).Bytes(),
+			defaultTTL,
+		).Err()
+		if redisSetErr != nil {
+			fmt.Printf("Failed to cache value for key %s\n", escapedQ)
+		}
+
+		return data, false, nil
+	} else if redisErr != nil {
+		return nil, false, redisErr
+	} else {
+		// Cached value exists. Extend TTL & build response
+		api.redisClient.Expire(ctx, escapedQ, defaultTTL).Result()
+		data := make([]NominatinLocation, 0)
+		unmarshalErr := json.Unmarshal(
+			bytes.NewBufferString(cacheValue).Bytes(), &data)
+		if unmarshalErr != nil {
+			return nil, false, unmarshalErr
+		}
+		return data, true, nil
+	}
 }
